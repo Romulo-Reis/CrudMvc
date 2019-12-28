5313930ec6f3b21373fd4159520c4d2c56a66438
Romulo-Reis <romuloreistavares@hotmail.com>
2019-12-28 20:09:16 -0300

    Criação da classe EscolaContext.

---
+++ CrudMvc/Models/EscolaContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CrudMvc.Models
+{
+    public class EscolaContext:DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}

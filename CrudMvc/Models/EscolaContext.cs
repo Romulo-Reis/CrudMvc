1a89e81914b12a7db132eb0c79200e284cf20a57
Romulo-Reis <romuloreistavares@hotmail.com>
2019-12-28 20:27:02 -0300

    Configurando a string de conexão.

--- CrudMvc/Models/EscolaContext.cs
+++ CrudMvc/Models/EscolaContext.cs
@@ -8,6 +8,10 @@ namespace CrudMvc.Models
 {
     public class EscolaContext:DbContext
     {
+        public EscolaContext():base("Escola")
+        {
+            Database.CreateIfNotExists();
+        }
         public DbSet<Aluno> Alunos { get; set; }
     }
 }
